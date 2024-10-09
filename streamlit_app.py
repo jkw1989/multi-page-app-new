497a2799fa4e53b9928af25ab486ced11758b451
jkw1989 <jameskingwood89@gmail.com>
2024-10-09 10:36:53 -0700

    good

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+import streamlit as st 
+
+st.title("New Multi Page App")
+
+# --- Page Setup 
+
+about_page = st.Page(
+    page="Views/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True,
+)
+project_1_page = st.Page(
+    page="Views/sales_dashboard.py",
+    title="Sales Dashboard",
+    icon=":material/bar_chart:",
+)
+project_2_page = st.Page(
+    page="Views/chatbot.py",
+    title="Chat Bot",
+    icon=":material/smart_toy:",
+)
+
+# --- Navigation
+
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page, project_2_page],
+    }
+)
+pg.run()
